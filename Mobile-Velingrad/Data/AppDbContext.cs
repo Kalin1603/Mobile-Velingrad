5ad00610a824c9667a5a2680e7cd94ab8f5ceec9
Kalin1603 <kalintodorov03@gmail.com>
2024-05-02 13:00:34 +0300

    Added models to DbContext

--- Mobile-Velingrad/Data/AppDbContext.cs
+++ Mobile-Velingrad/Data/AppDbContext.cs
@@ -1,13 +1,57 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Mobile_Velingrad.Data.Models;
 
 namespace Mobile_Velingrad.Data
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<User, IdentityRole<string>,string>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
+        }
+
+        public virtual DbSet<Brand> Brands { get; set; }
+
+        public virtual DbSet<Model> Models { get; set; }
+
+        public virtual DbSet<City> Cities { get; set; }
+
+        public virtual DbSet<Color> Colors { get; set; }
+
+        public virtual DbSet<Country> Countries { get; set; }
+
+        public virtual DbSet<Engine> Engines { get; set; }
+
+        public virtual DbSet<EngineType> EngineTypes { get; set; }
+
+        public virtual DbSet<ExtrasPackage> Extras { get; set; }
+
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        public virtual DbSet<TagCars> TagCars { get; set; }
+
+        public virtual DbSet<Vehicle> Vehicles { get; set; }
+
+        public virtual DbSet<VehicleType> VehicleTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TagCars>(entity =>
+            {
+                entity.HasKey(x => new { x.VehicleId, x.TagId });
+            });
+
+            builder.Entity<ExtrasPackage>(entity =>
+            {
+                entity.HasOne(e => e.Vehicle).WithOne(p => p.ExtrasPackage).OnDelete(DeleteBehavior.Restrict);
+            });
         }
     }
 }
---
+++ Mobile-Velingrad/Data/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mobile_Velingrad.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public User()
+        {
+            EmailConfirmed = true;
+        }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        [Display(Name = "Име")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [MinLength(0)]
+        [Display(Name = "Единен граждански номер")]
+        public string NationalId { get; set; }
+
+        public string Address { get; set; }
+    }
+}
